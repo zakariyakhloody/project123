9ea1037fc32c3d67d06d1fdb78e1737c7e99bf92
zakariyakhloody <zakariyakhloody@gmail.com>
2025-03-14 15:13:35 +0000

    Add 10.ts

---
+++ src/10.ts
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function Project123() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
