c432dc1c7268fc3eb6b0c9a508fd40e83700e7fe
zakariyakhloody <zakariyakhloody@gmail.com>
2025-03-18 15:11:02 +0000

    Add 14.ts

---
+++ src/14.ts
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Project123</title>
+  </head>
+  <body>
+    <h1>Welcome to Project123!</h1>
+    <p>This is a school project for my coursework.</p>
+    <form action="/action_page.php">
+      <label for="fname">First Name:</label><br>
+      <input type="text" id="fname" name="firstname"><br><br>
+      <label for="lname">Last Name:</label><br>
+      <input type="text" id="lname" name="lastname"><br><br>
+      <input type="submit" value="Submit">
+    </form> 
+  </body>
+</html>
