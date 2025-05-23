83e580c3e47c89d2727d2851be289c421f792db3
zakariyakhloody <zakariyakhloody@gmail.com>
2025-05-23 06:57:01 +0000

    Add 47.ts

---
+++ src/47.ts
@@ -0,0 +1,8 @@
+function getRandomString(length: number): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return result;
+}
